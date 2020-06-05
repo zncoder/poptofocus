df93db45a90f671336f6f89c694cf871321da329
Bin Zhang <binz@stripe.com>
2020-06-05 14:35:57 -0700

    pop all in

--- background.js
+++ background.js
@@ -30,15 +30,29 @@ function handleMenu(info, tab) {
   case "pop-in":
     popIn(info, tab)
     break
+  case "pop-all-in":
+    popAllIn()
+    break
   }
 }
 
 function popIn(info, tab) {
+  popInTab(tab)
+}
+
+function popInTab(tab) {
   chrome.tabs.create({
-    url: info.pageUrl,
+    url: tab.url,
     active: true,
   })
   chrome.tabs.remove(tab.id)
+}
+
+async function popAllIn() {
+  let tabs = await getAllTabs()
+  for (let t of tabs) {
+    popInTab(t)
+  }
 }
 
 function getDisplayInfo() {
@@ -51,6 +65,12 @@ function getDisplayInfo() {
   }
 }
 
+function getAllTabs() {
+  return new Promise(resolve => {
+    chrome.tabs.query({windowType: "popup"}, tabs => resolve(tabs))
+  })
+}
+
 async function init() {
   try {
     let info = await getDisplayInfo()
@@ -60,6 +80,7 @@ async function init() {
   }
 
   chrome.contextMenus.create({id: "pop-in", title: "Pop In", contexts: ["page"]})
+  chrome.contextMenus.create({id: "pop-all-in", title: "Pop All In", contexts: ["browser_action"]})
   chrome.contextMenus.onClicked.addListener(handleMenu)
   chrome.browserAction.onClicked.addListener(popUpTab)
 }
